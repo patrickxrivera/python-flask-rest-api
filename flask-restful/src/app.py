0ce4c725accbd124deee4056d1b2c436d568a90d
Patrick Rivera <pxr13@my.fsu.edu>
2018-08-13 20:29:46 -0700

    Finished flask-restful section

--- flask-restful/src/app.py
+++ flask-restful/src/app.py
@@ -1,29 +1,61 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identify
 
 app = Flask(__name__)
+app.secret_key = 'patrick'
 api = Api(app)
+jwt = JWT(app, authenticate, identify)
 
 items = []
 
 
 def get_item_by(name):
-    return next(list(filter(lambda i: i['name'] == name, None)))
+    return next((i for i in items if i['name'] == name), None)
+
+
+def create_item(name, data):
+    return {'name': name, 'price': data['price']}
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help='This field cannot be blank!'
+                        )
+
+    @jwt_required()
     def get(self, name):
         item = get_item_by(name)
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        data = request.get_json()
         item_exists = get_item_by(name)
         if item_exists:
             return {'error': 'Item with name {} already exists'.format(name)}, 400
-        item = {'name': name, 'price': data['price']}
+        data = Item.parser.parse_args()
+        item = create_item(name, data)
         items.append(item)
         return item, 201
+
+    def delete(self, name):
+        global items
+        items = [item for item in items if item['name'] != name]
+        return {'message': 'Item deleted.'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = get_item_by(name)
+        if item:
+            item.update(data)
+        else:
+            item = create_item(name, data)
+            items.append(item)
+        return item
 
 
 class ItemList(Resource):
---
+++ flask-restful/src/security.py
@@ -0,0 +1,19 @@
+from user import User
+
+users = [
+    User(1, 'patrick', 'tuna')
+]
+
+username_mapping = {u.username: u for u in users}
+user_id_mapping = {u.id: u for u in users}
+
+
+def authenticate(username, password):
+    user = username_mapping.get(username, None)
+    if user and user.password == password:
+        return user
+
+
+def identify(payload):
+    user_id = payload['identity']
+    return user_id_mapping.get(user_id, None)
