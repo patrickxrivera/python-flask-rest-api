0d9fef6fe402502f699f0a3b834e7b0f758067c9
Patrick Rivera <pxr13@my.fsu.edu>
2018-08-14 21:48:33 -0700

    finished

--- sql-alchemy/src/app.py
+++ sql-alchemy/src/app.py
@@ -8,9 +8,9 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['PROPAGATE_EXCEPTIONS'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
 api = Api(app)
 
@@ -20,7 +20,7 @@ def create_tables():
     db.create_all()
 
 
-jwt = JWT(app, authenticate, identity)
+jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
@@ -31,4 +31,4 @@ api.add_resource(UserRegister, '/register')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(debug=True)  # important to mention debug=True
+    app.run(port=5000, debug=True)
